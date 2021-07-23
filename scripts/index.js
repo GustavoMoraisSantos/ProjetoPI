cf3319613d70fc1b433bdd84761a753fe23d3ab3
Gustavo Morais Lopes dos Santos <gustavo.morais1414@gmail.com>
2021-07-23 10:13:09 -1000

    Arrumei o menu dropdown e o slider
    o menu não estava funcionando nas outras páginas e o slider não estava ocupando 100% da tela

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const dropDown = document.getElementById('dropdown-mobile')
 
 function openDropDownMobile() {
+    console.log("qualquer coisa", dropDown.classList)
     if (dropDown.classList == 'menu__dropdown-mobile--hidden') {
         dropDown.classList = 'menu__dropdown-mobile--visible'
     } else {
